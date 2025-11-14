3428d3d58bbce7ca0613f82eae5c54954755302a
Guhan1240 <sguhan635@gmail.com>
2025-11-14 10:47:25 +0530

    second

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend-jwt-e2ai.onrender.com",
+  baseURL: "https://express-jwt-8xp4.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
